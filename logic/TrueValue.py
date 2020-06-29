156a17aa2ade06a53d9f2f2c66be126dc5224ec6
XIAZY <i@mechanus.io>
2020-06-29 05:42:07 -0400

    add nth_derivative_from_string

--- logic/TrueValue.py
+++ logic/TrueValue.py
@@ -1,8 +1,13 @@
-from sympy import diff
+from sympy import diff, symbols
 from sympy.parsing.sympy_parser import parse_expr
 from sympy.parsing.sympy_parser import standard_transformations, implicit_multiplication_application
-def nth_derivative(stringExpr, var, n, value):
+
+def nth_derivative_from_string(stringExpr, stringVar, n, value):
     transformations = (standard_transformations + (implicit_multiplication_application,))
     formula = parse_expr(stringExpr, transformations = transformations)
+    var = symbols(stringVar)
+    return nth_derivative(formula, var, n, value)
+
+def nth_derivative(formula, var, n, value):
     derivative = diff(formula, var, n)
     return derivative.subs(var,value)
