b993e838347089d700c6115f0b7412073351ce2c
Spencer McMurray <spencermcmurray@yahoo.ca>
2020-07-25 15:40:58 -0400

    added frontend integration

--- components/GraphBox.tsx
+++ components/GraphBox.tsx
@@ -4,17 +4,18 @@ import { Approximation } from "../public/static/helpers/interfaces";
 
 interface GraphBoxPorps {
   x: number[];
-  lines: Approximation[];
+  y: number[];
+  approxs: Approximation[];
   show: string;
 }
 
-const GraphBox: FunctionComponent<GraphBoxPorps> = ({ x, lines, show }) => {
-  let approxLines = lines.map((l) => ({ name: l.name, data: l.y }));
-  let errLines = lines.map((l) => ({ name: l.name, data: l.error }));
+const GraphBox: FunctionComponent<GraphBoxPorps> = ({ x, approxs, show }) => {
+  let approxLines = approxs.map((a) => ({ name: a.name, data: a.y }));
+  let errLines = approxs.map((a) => ({ name: a.name, data: a.error }));
 
   if (show.length > 0) {
-    approxLines = approxLines.filter((l) => l.name === show);
-    errLines = errLines.filter((l) => l.name === show);
+    approxLines = approxLines.filter((a) => a.name === show);
+    errLines = errLines.filter((a) => a.name === show);
   }
 
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,20 +10,10 @@ import Errors from "../components/Errors";
 const Index: NextPage<{}> = () => {
   const [show, setShow] = useState("");
   const [errs, setErrs] = useState([]);
+  const [x, setX] = useState<number[]>([]);
+  const [y, setY] = useState<number[]>([]);
+  const [approxs, setApproxs] = useState<Approximation[]>([]);
   // TODO: These consts will come from a backend fetch
-  const x: number[] = [1, 2, 3];
-  const lines: Approximation[] = [
-    {
-      name: "Approx A",
-      y: [4, 3, 2],
-      error: [14, 2, 0],
-    },
-    {
-      name: "Approx B",
-      y: [3, 4, 3],
-      error: [0, 15, 4],
-    },
-  ];
   return (
     <React.Fragment>
       <Title />
@@ -31,14 +21,19 @@ const Index: NextPage<{}> = () => {
         <Errors errs={errs} />
       </div>
       <div className="container">
-        <InputBox setErrs={setErrs} />
+        <InputBox
+          setX={setX}
+          setY={setY}
+          setApproxs={setApproxs}
+          setErrs={setErrs}
+        />
         <SelectBox
           show={show}
           setShow={setShow}
-          names={lines.map((l) => l.name)}
+          names={approxs.map((a) => a.name)}
         />
       </div>
-      <GraphBox x={x} lines={lines} show={show} />
+      <GraphBox x={x} y={y} approxs={approxs} show={show} />
     </React.Fragment>
   );
 };
