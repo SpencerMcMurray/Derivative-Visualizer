b78f98b2f62e810f72f6793b2fe6c75cd4ee2448
AlAndr04 <aandros04@gmail.com>
2020-08-10 00:36:13 -0400

    Uses the original function, but with constant h

--- server/logic/newton_implementation.py
+++ server/logic/newton_implementation.py
@@ -20,7 +20,8 @@ def choose(n, k):
 
 def nth_derivative (f, x, n):
     x = float_type(x)
-    h = np.multiply(np.sqrt(np.finfo(float_type).eps), x)
+    #h = np.multiply(np.sqrt(np.finfo(float_type).eps), x)
+    h = float_type(np.float_power(10,-5))
     _sum = float_type(0)
     for k in range(n+1):
         if (k + n) % 2 == 0:
@@ -29,19 +30,18 @@ def nth_derivative (f, x, n):
             _const = float_type(-1 * choose(n, k))
         summand = np.multiply(_const, f(np.add(float_type(x), np.multiply(float_type(k), h))))
         _sum = np.add(_sum, summand)
-    print("Sum", _sum)
     h_to_n = np.power(h, n)
     print(h)
     return np.multiply(np.divide(float_type(1), h_to_n), _sum)
 
 def new_nth_derivative(f,x,n):
-    h = np.multiply(np.sqrt(np.finfo(float_type).eps), float_type(x))
+    h = 0.0001
     s = 0
     for k in range(n+1):
         s += ((-1)**(k+n))*choose(n,k)*f(x+k*h)
-    return s/h**n
+    return s/np.power(h,n)
 
 if __name__ == "__main__":
     # debug code
-    print(nth_derivative(lambda x: (x**3), 5, 2))
-    print(new_nth_derivative(lambda x: np.sin(x), np.pi, 3))
+    print(nth_derivative(lambda x: (np.cos(x)), np.pi/2, 1))
+    print(new_nth_derivative(lambda x: (np.cos(x)), np.pi/2, 1))
